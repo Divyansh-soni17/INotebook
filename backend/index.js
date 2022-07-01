6a84c111487d806c0921d18d94fdba27b5c14d20
Divyansh <sonihoneysoni6@gmail.com>
2022-07-01 10:32:08 +0530

    ADDED DATA

--- backend/index.js
+++ backend/index.js
@@ -2,15 +2,13 @@ const connectToMongo = require("./db");
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
+const dotenv = require("dotenv");
 
 connectToMongo();
 
 const app = express();
 
-const port = process.env.PORT;
-if (process.env.NODE_ENV !== "PRODUCTION") {
-  require("dotenv").config({ path: "backend/config/config.env" });
-}
+const port = 5000;
 
 app.use(cors());
 
@@ -19,11 +17,12 @@ app.use(express.json());
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/notes", require("./routes/notes"));
 
-
-// app.use(express.static(path.join,(__dirname,"../build")));
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, '../build/index.html'))
-// });
+if (process.env.NODE_ENV === "PRODUCTION") {
+  app.use(express.static((__dirname, "../build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../build/index.html"));
+  });
+}
 
 app.listen(port, () => {
   console.log(`iNotebook backend  listening at http://localhost:${port}`);
