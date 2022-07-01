1bfb76b40c85d376c651f703a0b94cd07bce6fee
Divyansh <sonihoneysoni6@gmail.com>
2022-07-01 10:49:05 +0530

    Updated

--- backend/index.js
+++ backend/index.js
@@ -17,12 +17,12 @@ app.use(express.json());
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/notes", require("./routes/notes"));
 
-if (process.env.NODE_ENV === "PRODUCTION") {
+ 
   app.use(express.static((__dirname, "../build")));
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../build/index.html"));
   });
-}
+ 
 
 app.listen(port, () => {
   console.log(`iNotebook backend  listening at http://localhost:${port}`);
